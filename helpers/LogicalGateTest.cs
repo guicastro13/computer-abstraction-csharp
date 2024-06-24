39f011ed60063705fad06539077d386369797cfe
Guilherme Castro <guii_1@hotmail.com>
2024-06-24 17:27:00 -0300

    create tests

--- helpers/LogicalGateTest.cs
+++ helpers/LogicalGateTest.cs
@@ -15,7 +15,16 @@ namespace LogicalGateTests
 
             Assert.That(result, Is.EqualTo(expected));
         }
+        [Test]
+        public void GateNot_ReturnsWrongResult()
+        {
+            Bit a = Bit.One;
+            Bit expected = Bit.One;
 
+            Bit result = Gates.Not(a);
+
+            Assert.That(result, Is.Not.EqualTo(expected));
+        }
         [Test]
         public void GateAnd_ReturnsCorrectResult()
         {
@@ -27,7 +36,17 @@ namespace LogicalGateTests
 
             Assert.That(result, Is.EqualTo(expected));
         }
+        [Test]
+        public void GateAnd_ReturnWrongResult()
+        {
+            Bit a = Bit.One;
+            Bit b = Bit.One;
+            Bit expected = Bit.Zero;
 
+            Bit result = Gates.And(a, b);
+
+            Assert.That(result, Is.Not.EqualTo(expected));
+        }
         [Test]
         public void Or_ReturnsCorrectResult()
         {
@@ -40,6 +59,17 @@ namespace LogicalGateTests
             Assert.That(result, Is.EqualTo(expected));
         }
         [Test]
+        public void Or_ReturnsWrongResult()
+        {
+            Bit a = Bit.One;
+            Bit b = Bit.Zero;
+            Bit expected = Bit.Zero;
+
+            Bit result = Gates.Or(a, b);
+
+            Assert.That(result, Is.Not.EqualTo(expected));
+        }
+        [Test]
         public void GateXor_ReturnsCorrectResult()
         {
             Bit a = Bit.One;
@@ -50,51 +80,82 @@ namespace LogicalGateTests
 
             Assert.That(result, Is.EqualTo(expected));
         }
-
         [Test]
-        public void GateAnd_WithDifferentBits_ReturnsZero()
+        public void GateXor_ReturnsWrongResult()
         {
             Bit a = Bit.One;
             Bit b = Bit.Zero;
             Bit expected = Bit.Zero;
 
-            Bit result = Gates.And(a, b);
+            Bit result = Gates.Xor(a, b);
+
+            Assert.That(result, Is.Not.EqualTo(expected));
+        }
+        [Test]
+        public void GateXnor_ReturnsCorrectResult()
+        {
+            Bit a = Bit.One;
+            Bit b = Bit.Zero;
+            Bit expected = Bit.Zero;
+
+            Bit result = Gates.Xnor(a, b);
 
             Assert.That(result, Is.EqualTo(expected));
         }
-
         [Test]
-        public void GateOr_WithDifferentBits_ReturnsOne()
+        public void GateXnor_ReturnsWrongResult()
         {
             Bit a = Bit.One;
             Bit b = Bit.Zero;
             Bit expected = Bit.One;
 
-            Bit result = Gates.Or(a, b);
+            Bit result = Gates.Xnor(a, b);
 
-            Assert.That(result, Is.EqualTo(expected));
+            Assert.That(result, Is.Not.EqualTo(expected));
         }
-
         [Test]
-        public void GateNot_WithZero_ReturnsOne()
-        {
-            Bit a = Bit.Zero;
-            Bit expected = Bit.One;
-
-            Bit result = Gates.Not(a);
-
-            Assert.That(result, Is.EqualTo(expected));
-        }
-
-        [Test]
-        public void GateNot_WithOne_ReturnsZero()
+        public void GateNand_ReturnsCorrectResult()
         {
             Bit a = Bit.One;
+            Bit b = Bit.One;
             Bit expected = Bit.Zero;
 
-            Bit result = Gates.Not(a);
+            Bit result = Gates.Nand(a, b);
 
             Assert.That(result, Is.EqualTo(expected));
+        }
+        [Test]
+        public void GateNand_ReturnsWrongResult()
+        {
+            Bit a = Bit.One;
+            Bit b = Bit.One;
+            Bit expected = Bit.One;
+
+            Bit result = Gates.Nand(a, b);
+
+            Assert.That(result, Is.Not.EqualTo(expected));
+        }
+        [Test]
+        public void GateNor_ReturnsCorrectResult()
+        {
+            Bit a = Bit.One;
+            Bit b = Bit.Zero;
+            Bit expected = Bit.Zero;
+
+            Bit result = Gates.Nor(a, b);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }  
+        [Test]
+        public void GateNor_ReturnsWrongResult()
+        {
+            Bit a = Bit.One;
+            Bit b = Bit.Zero;
+            Bit expected = Bit.One;
+
+            Bit result = Gates.Nor(a, b);
+
+            Assert.That(result, Is.Not.EqualTo(expected));
         }
     }
 }
